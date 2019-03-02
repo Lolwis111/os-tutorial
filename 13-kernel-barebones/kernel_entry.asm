23270d120f2a40f7c8af0fc6e62b26d606396612
Lolwis111 <lolwissoftware@live.de>
2019-03-02 21:34:40 +0100

    Update kernel_entry.asm
    Added the _start label, removes linker warning

--- 13-kernel-barebones/kernel_entry.asm
+++ 13-kernel-barebones/kernel_entry.asm
@@ -1,4 +1,6 @@
 [bits 32]
 [extern main] ; Define calling point. Must have same name as kernel.c 'main' function
+[global _start] ; Define the _start label to remove the linker warning
+_start:         ; the _start label in question
 call main ; Calls the C function. The linker will know where it is placed in memory
 jmp $
